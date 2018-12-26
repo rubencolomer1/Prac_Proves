b1ad735930541bfc10cb7da987b1e2e11d9e6311
rubencolomer1 <rubencolomer.1@gmail.com>
2018-12-26 22:27:54 +0100

    Second commit

--- src/data/Party.java
+++ src/data/Party.java
@@ -1,4 +1,36 @@
 package data;
 
-public class Party {
+public class Party
+{
+    private final String name;
+    public Party(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Party party1 = (Party) o;
+        return name.equals(party1.name);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Party{" + "name='" + name + '\'' + '}';
+    }
 }
+
