8e5fbe738ad61603eb272b72b6a427e8bc782c7e
rubencolomer1 <rubencolomer.1@gmail.com>
2018-12-27 14:44:58 +0100

    getter digitalsignature

--- src/data/DigitalSignature.java
+++ src/data/DigitalSignature.java
@@ -4,8 +4,14 @@ import java.util.Arrays;
 final public class DigitalSignature
 {
     private final byte[] digitalSignature;
+
     public DigitalSignature(byte[] digitalSignature)
     {
         this.digitalSignature = digitalSignature;
+    }
+
+    public byte[] getDigitalSignature()
+    {
+        return digitalSignature;
     }
 }
