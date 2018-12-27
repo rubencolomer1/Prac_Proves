56f482faf766c3c44aecefaa88086eecf518fcb0
rubencolomer1 <rubencolomer.1@gmail.com>
2018-12-27 22:43:05 +0100

    tests1

---
+++ tests/data/PartyTest.java
@@ -0,0 +1,26 @@
+package data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartyTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void equals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hashCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toString() {
+    }
+}
