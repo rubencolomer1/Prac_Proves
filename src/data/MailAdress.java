053979d1c1fb75b4c92614f65eea87826de52ba7
rubencolomer1 <rubencolomer.1@gmail.com>
2018-12-27 18:09:34 +0100

    MailAdress

--- src/data/MailAdress.java
+++ src/data/MailAdress.java
@@ -1,4 +1,33 @@
 package data;
 
-public class MailAdress {
+final public class MailAdress
+{
+    private final String mail;
+
+    public MailAdress(String mail)
+    {
+        this.mail = mail;
+    }
+    public String getMailAdress()
+    {
+        return mail;
+    }
+    @Override
+    public String toString()
+    {
+        return "MailAdress{" + "mail ='" + mail + '\'' + '}';
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MailAdress mailAdress = (MailAdress) o;
+        return mailAdress.equals(mail);
+    }
+    @Override
+    public int hashCode()
+    {
+        return mail.hashCode();
+    }
 }
