d61254d22f4a24beade5770156639e6f32732b8f
rubencolomer1 <rubencolomer.1@gmail.com>
2018-12-28 18:12:33 +0100

    testedParty

--- tests/data/PartyTest.java
+++ tests/data/PartyTest.java
@@ -1,26 +1,45 @@
 package data;
 
+import Exceptions.NullException;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PartyTest {
-
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+class PartyTest
+{
+    private Party prova1, prova2;
+    {
+        try
+        {
+            prova1 = new Party("hola");
+            prova2 = new Party("hola");
+        } catch (NullException e) {
+            e.printStackTrace();
+        }
     }
 
     @org.junit.jupiter.api.Test
-    void getName() {
+    public void TestgetName()
+    {
+        assertEquals("hola", prova1.getName());
     }
 
     @org.junit.jupiter.api.Test
-    void equals() {
+    public void TesttoString()
+    {
+        String stringProva1 = "Party{name='hola'}";
+        assertEquals(stringProva1,prova1.toString());
     }
 
     @org.junit.jupiter.api.Test
-    void hashCode() {
+    public void TestEquals()
+    {
+        assertTrue(prova1.equals(prova2));
     }
 
     @org.junit.jupiter.api.Test
-    void toString() {
+    public void TestHashCode(){
+        assertTrue(prova1.hashCode()==prova2.hashCode());
     }
+
 }
