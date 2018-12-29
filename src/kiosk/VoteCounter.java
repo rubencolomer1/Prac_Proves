01bab357751a22b7abb27fdca9e02fccb0711c72
rozeoner <sergiovagaspuy@gmail.com>
2018-12-29 20:22:55 +0100

    getters votecounter

--- src/kiosk/VoteCounter.java
+++ src/kiosk/VoteCounter.java
@@ -1,16 +1,42 @@
 package kiosk;
+
+import data.Party;
+import java.util.Set;
+
 /**
  * A class that represents the result in an election site
  */
-public class VoteCounter {
- ???
-    public VoteCounter(Set<Party> validParties) { ??? }
-    public void countParty(Party party) { ??? }
-    public void countNull() { ??? }
-    public void countBlank() { ??? }
-    public void scrutinize(Party party) { ??? }
-    public int getVotesFor(Party party) { ??? }
-    public int getNulls() { ??? }
-    public int getBlanks() { ??? }
-    public int getTotal() { ??? }
+
+public class VoteCounter extends Party {
+
+   private int votes;
+   private int nulls;
+   private int blanks;
+   private int total;
+
+    public VoteCounter(Set<Party> validParties) { ???}
+
+    public void countParty(Party party) { ???}
+
+    public void countNull() { ???}
+
+    public void countBlank() { }
+
+    public void scrutinize(Party party) { ???}
+
+    public int getVotesFor(Party party) {
+      return votes;
+    }
+
+    public int getNulls() {
+      return nulls;
+    }
+
+    public int getBlanks() {
+      return blanks;
+    }
+
+    public int getTotal() {
+     return total;
+    }
 }
