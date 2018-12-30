5f05e806948cd06ac20e6fe1a59808ac9a5f9070
rubencolomer1 <rubencolomer.1@gmail.com>
2018-12-30 17:10:53 +0100

    1_VoteCounter

--- src/kiosk/VoteCounter.java
+++ src/kiosk/VoteCounter.java
@@ -14,19 +14,43 @@ public class VoteCounter {
    private int nulls;
    private int blanks;
    private int total;
+   private Set<Party> validParties;
 
     public VoteCounter(Set<Party> validParties)
     {
-
+        this.validParties = validParties;
     }
 
-    public void countParty(Party party) { ???}
+    public void countParty(Party party)
+    {
+        this.votes += 1;
+    }
 
-    public void countNull() { ???}
+    public void countNull()
+    {
+        this.nulls += 1;
+    }
 
-    public void countBlank() { }
+    public void countBlank()
+    {
+        this.blanks += 1;
+    }
 
-    public void scrutinize(Party party) { ???}
+    public void scrutinize(Party party)
+    {
+        if (validParties.contains(party.getName()))
+        {
+            countParty(party);
+        }
+        else if ( party.equals(""))
+        {
+            countBlank();
+        }
+        else
+        {
+            countNull();
+        }
+    }
 
     public int getVotesFor(Party party) {
       return votes;
