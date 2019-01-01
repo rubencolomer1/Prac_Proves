4151124bba15fa941b03f157d8ed3f461fb1a11b
rubencolomer1 <rubencolomer.1@gmail.com>
2019-01-01 22:07:17 +0100

    VotingKiosk_2

--- src/kiosk/VotingKiosk.java
+++ src/kiosk/VotingKiosk.java
@@ -1,31 +1,28 @@
 package kiosk;
 
+import Exceptions.NullException;
 import Exceptions.NullPartyException;
 import data.*;
 import  services.*;
-
-import java.util.Set;
-
 
 public abstract class VotingKiosk
 {
     private VoteCounter v1;
     private String nif;
-    private boolean potVotar;
-    private boolean volRebut;
+    private boolean haVotat;
+    private boolean votEscrutat;
     private Party opcioVot;
 
 
     private MailerService mService;
     private ElectoralOrganism eO;
 
-    private Set<Party> validParties;
 
-    VoteCounter votecounter = new VoteCounter(validParties);
 
     public VotingKiosk()
     {
-
+        this.haVotat = false;
+        this.votEscrutat = false;
     }
 
     public void setElectoralOrganism(ElectoralOrganism eO)
@@ -40,12 +37,35 @@ public abstract class VotingKiosk
 
     public void vote(Party party) throws NullPartyException
     {
-        votecounter.scrutinize(party);
+        if (haVotat == true || votEscrutat == true)
+        {
+            throw new IllegalStateException();
+        }
+        else
+        {
+            this.opcioVot = party;
+            v1.scrutinize(opcioVot);
+            votEscrutat = true;
+            haVotat = true;
+        }
     }
 
-    public void sendeReceipt(MailAdress address)
-    {
+    public void sendeReceipt(MailAdress address) throws NullException {
+        if (!haVotat)
+        {
+            throw new IllegalStateException();
+        }
+        else if (haVotat == false && votEscrutat == false)
+        {
+            throw new IllegalStateException();
+        }
 
+        else
+        {
+            DigitalSignature digitalSignature = eO.askForDigitalSignature(opcioVot);
+            mService.send(address, digitalSignature);
+            haVotat = false;
+        }
     }
 
 }
