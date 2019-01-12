10c39405c56cf3778a9525f8047d4e81287704f6
rubencolomer1 <rubencolomer.1@gmail.com>
2019-01-12 22:04:44 +0100

    Biometric_1

--- src/data/BiometricData.java
+++ src/data/BiometricData.java
@@ -1,30 +1,34 @@
 package data;
 
 import Exceptions.NoFacialPointsException;
+import services.BiometricReader;
+import services.BiometricScanner;
+import services.BiometricSoftware;
 
 import java.math.BigInteger;
 
-public class BiometricData {
+public class BiometricData
+{
     //facial points
     public BigInteger facialKey;
     public BigInteger fingerPrint;
+    private BiometricScanner bS;
+    private BiometricReader bR;
+    private BiometricSoftware bSW;
 
-            public BiometricData(BigInteger FacialKey,BigInteger fingerPrint) throws NoFacialPointsException {
-                if (facialKey == null || fingerPrint == null){
-                    throw new NoFacialPointsException("Facial point cannot be null!");
-                } else {
-                    this.facialKey = facialKey;
-                    this.fingerPrint = fingerPrint;
-                }
-            }
-
-    public BigInteger getFacialKey() {
-        return facialKey;
+    public BiometricData(BigInteger FacialKey,BigInteger fingerPrint) throws NoFacialPointsException
+    {
+        if (facialKey == null || fingerPrint == null)
+        {
+            throw new NoFacialPointsException("Facial point cannot be null!");
+        }
+        else
+        {
+            this.facialKey = facialKey;
+            this.fingerPrint = fingerPrint;
+        }
     }
 
-    public BigInteger getFingerPrint () {
-        return fingerPrint;
-    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,6 +40,26 @@ public class BiometricData {
     @Override
     public String toString() {
         return "BiometricData{" + "facial='" + this.facialKey + '\'' + ", fingerprint='" + this.fingerPrint + '\'' +'}';
+    }
+
+    public void setBiometricScanner(BiometricScanner bS)
+    {
+        this.bS = bS;
+    }
+    public void setBiometricReader(BiometricReader bR)
+    {
+        this.bR = bR;
+    }
+
+    public void setBiometricSoftware(BiometricSoftware bSW)
+    {
+        this.bSW = bSW;
+    }
+
+    public void getBiometricFacials()
+    {
+        this.facialKey = bS.scanFace();
+        this.fingerPrint = bS.scanFingerprint();
     }
 
 }
